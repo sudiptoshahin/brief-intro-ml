77249ed3cf36a0ee7f3e8309cc0414eb40bb70fd
sudipta.shahin <sudipta.shahin@dreamonline.co.jp>
2023-12-11 17:26:25 +0600

    c and line profiler for profiling

--- profiling/Profiling.py
+++ profiling/Profiling.py
@@ -9,6 +9,23 @@ from io import StringIO
 import cProfile
 from line_profiler import LineProfiler
 
-class Profiling():
 
-    # def __init__(self, ):
+class Profiling:
+
+    """
+        # profiler is a type of profiling method like (CProfiler, line_profiler)
+        # functions is a list of function or single function reference
+        # this @Profiling class needs to instantiate with these parameters
+    """
+    """
+        * pass a single function / list of function
+        * function list can be used only in line profiler
+        * method/profiler = line_profiler or CProfiler
+        * Generate CSV reports
+    """
+    def __init__(self, profiler, functions):
+        self.profiler = profiler
+        self.functions = functions
+
+
+
