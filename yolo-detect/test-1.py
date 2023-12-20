fd7e9ee516e98859ee9f74795987bca47534de4b
sudipta.shahin <sudipta.shahin@dreamonline.co.jp>
2023-12-20 14:46:18 +0600

    yolo and cv2 obj detect

---
+++ yolo-detect/test-1.py
@@ -0,0 +1,13 @@
+import torch
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+img = 'https://ultralytics.com/images/zidane.jpg'
+
+results = model(img)
+# plt.imshow(np.squeeze(results.render()))
+# plt.show()
+print(results.xyxy)
